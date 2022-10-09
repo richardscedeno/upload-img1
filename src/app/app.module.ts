cbe7386a1ac632442faa9e80137a20ba56a50a8b
Richards Cedeño <chards.rjcov@gmail.com>
2022-10-09 01:47:54 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { Camera } from '@ionic-native/camera/ngx';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [Camera, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Camera, CameraOptions  } from '@ionic-native/camera/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,86 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  picture:any[]=[];
+  base64Image:any;
+
+  constructor(private camera:Camera) {
+  }
+
+  AccessCamera(){
+    this.camera.getPicture({
+
+      targetWidth:400,   
+      targetHeight:200,   
+      correctOrientation:true,   
+      sourceType: this.camera.PictureSourceType.CAMERA,   
+      destinationType: this.camera.DestinationType.DATA_URL
+   
+    }).then((imageData) => {
+      this.base64Image = "data:image/jpeg;base64,"+imageData;
+      this.picture = imageData;
+    },(err) => {
+      console.log(err);
+    });
+  }
+   
+  AccessGallery(){
+   this.camera.getPicture({
+  
+    sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+    destinationType: this.camera.DestinationType.DATA_URL
+  
+   }).then((imageData) => {
+     this.base64Image = "data:image/jpeg;base64,"+imageData;
+     this.picture = imageData;
+   },(err) => {
+     console.log(err);
+   });
+  }
+
+  // Upload(){
+  //   var headers = new Headers();
+  //   headers.append("Accept", 'application/json');
+  //   headers.append('Content-Type', 'application/json' );
+
+  //   let options = new RequestOptions({ headers: headers });  
+  //   let data = {
+  //     image:this.base64Image
+  //   };
+  
+  //   let loader = this.loading.create({
+  
+  //     content: 'Processing please wait…',
+  
+  //   });
+  
+  //   loader.present().then(() => {
+    
+  //     this.http.post('http://ionicdon.com/mobile/upload_data.php',data,options).map(res => res.json())
+  //     .subscribe(res => {
+  //       loader.dismiss()
+
+  //       if(res=="Successfully_Uploaded"){
+        
+  //         let alert = this.alertCtrl.create({
+  //           title:"CONGRATS",
+  //           subTitle:(res),
+  //           buttons: ['OK']
+  //         });
+        
+  //         alert.present();
+
+  //       }else {
+        
+  //         let alert = this.alertCtrl.create({
+  //           title:"ERROR",
+  //           subTitle:"Image could not be uploaded",
+  //           buttons: ['OK']
+  //         });
+  //         alert.present();
+  //       }
+  //     });
+  //   });
+  // }
 
 }
